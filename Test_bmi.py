b38a73d59d43c4703923eb15ae1e93057e6456b1
Drew <drewcfrog@gmail.com>
2023-10-31 10:46:21 +0800

    added test for bmi calculator

---
+++ Test_bmi.py
@@ -0,0 +1,13 @@
+import Lab2_DOAIOT.bmi as bmi
+
+def test_bmi_normal_weight():
+    result = bmi.calculate_bmi(1.73,57)
+    assert(result == 0)
+
+def test_bmi_over_weight():
+    result = bmi.calculate_bmi(1.73,100000)
+    assert(result == 1)
+
+def test_bmi_under_weight():
+    result = bmi.calculate_bmi(1.73,1)
+    assert(result == -1)
